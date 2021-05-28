9ac2c00a8a30a697be9dc34c47ede1d75baa26c4
diegolop08 <diegolopezg01@hotmail.com>
2021-05-27 22:58:17 -0500

    voids eliminated & constrins updated

--- Placa2d.m
+++ Placa2d.m
@@ -18,8 +18,8 @@ d = 0;
 constrains =  [1:32 33 64 65 96 97 128 129 160 161 192 193 224 225 256 257 ...
               288 289 320 321 352 353 384 385 416 417 448 449 480 481 512 ...
               513 544 545 576 577 608 609 640 641 672 673 704 705 734 735 ...
-              764 765 794 795 822 823 850 851 876 877 900 901 922 923 942 ...
-              943 958 959 970:978];
+              764 765 794 795 822 823 850 851 876 877 900 901 922 923 924 ...
+              941 942 943 944 957 958 959 960 969 970:978];
 
 while d<0.1
   q = q+0.01;
@@ -40,18 +40,10 @@ function x = solver(B)
     if ~ismember(i,constrains)
       A(i,i) = -4;%center
       sector = locator(i);
-      if ~isVoidRight(i,sector)
-        A(i,i+1) = 1;%right
-      end
-      if ~isVoidAbove(i,sector)
-        A(i, i+stepToAbove(i, sector)) = 1; %above
-      end
-      if ~isVoidLeft(i,sector)
-        A(i,i-1) = 1;%left
-      end
-      if ~isVoidBelow(i,sector)
-        A(i, i-stepToBelow(i, sector)) = 1; %below
-      end
+      A(i,i+1) = 1;%right
+      A(i, i+stepToAbove(i, sector)) = 1; %above
+      A(i,i-1) = 1;%left
+      A(i, i-stepToBelow(i, sector)) = 1; %below
     end
   end
   A(constrains,:) = [];
@@ -81,56 +73,6 @@ function sector = locator(i)
     sector = 9;
   else %sector 10
     sector = 10;
-  end
-end
-
-function out = isVoidRight(i, sector)
-  switch sector
-    case 1
-      out = mod(i,32) == 0;
-    case 2
-      out = ismember(i,734:30:794);
-    case 3
-      out = ismember(i,[822 850]);
-    case 4
-      out = i==876;
-    case 5
-      out = i==900;
-    case 6
-      out = i==922;
-    case 7
-      out = i==942;
-    case 8
-      out = i==958;
-    case 9
-      out = i==970;
-    case 10
-      out = i==978;
-  end
-end
-
-function out = isVoidAbove(i, sector)
-  switch sector
-    case 1
-      out = ismember(i,[673 704]);
-    case 2
-      out = ismember(i,[765 794]);
-    case 3
-      out = ismember(i,[823 850]);
-    case 4
-      out = ismember(i,[851 876]);
-    case 5
-      out = ismember(i,[877 900]);
-    case 6
-      out = ismember(i,[901 922]);
-    case 7
-      out = ismember(i,[923 924 941 942]);
-    case 8
-      out = ismember(i,[943 944 957 958]);
-    case 9
-      out = ismember(i,[959 960 969 970]);
-    case 10
-      out = 1;
   end
 end
 
@@ -166,40 +108,6 @@ function step = stepToAbove(i, sector)
     case 9
         step = 10;
    end
-end
-
-function out = isVoidLeft(i, sector)
-  switch sector
-    case 1
-      out = ismember(i,1:32:673);
-    case 2
-      out = ismember(i,705:30:765);
-    case 3
-      out = ismember(i,[795 822]);
-    case 4
-      out = i==851;
-    case 5
-      out = i==877;
-    case 6
-      out = i==901;
-    case 7
-      out = i==923;
-    case 8
-      out = i==943;
-    case 9
-      out = i==959;
-    case 10
-      out = i==971;
-  end
-end
-
-function out = isVoidBelow(i, sector)
-  switch sector
-    case 1
-      out = ismember(i,2:31);
-    otherwise
-      out = false;
-  end
 end
 
 function step = stepToBelow(i, sector)
